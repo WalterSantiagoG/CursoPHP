ada9ac769402775065ad0cec3ec96159f5e4f839
Walter Santiago <wsantiagog1@gmail.com>
2018-04-29 18:49:58 -0500

    Clase 6 Arreglos

---
+++ variables/arrays.php
@@ -0,0 +1,27 @@
+<?php 
+
+/*$arrayVar = ['red', 'blue', 'black'];
+var_dump($arrayVar);*/
+
+/*$arrayVar = [
+	0 => 'red',
+  	1 => 'blue',
+  	2 => 'black'
+];
+var_dump($arrayVar);*/
+
+/*$arrayVar = [
+	3 => 'red',
+  	1 => 'blue',
+  	2 => 'black'
+];
+var_dump($arrayVar);*/
+
+$arrayVar = [
+	'color1' => 'red',
+  	'color2' => 'blue',
+  	3 => 'black'
+];
+var_dump($arrayVar['color1']);
+
+?>
