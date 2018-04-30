8dc181bbe055d0d4917cb89b53020d97dc4e797a
Walter Santiago <wsantiagog1@gmail.com>
2018-04-30 13:36:25 -0500

    Clase 17 Cargas de Archivos Externos

---
+++ external/functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo '<p>Text from functions.php</p>';
+
+function sum($a, $b){
+	$result = $a + $b;
+	echo '<p>Result: ' . $result . '</p>';
+}
+
+?>
---
+++ external/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+require 'functions1.php';
+
+echo '<p>Text from index.php</p>';
+
+sum(10,20);
+
+?>
