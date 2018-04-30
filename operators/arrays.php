13d7fae3843f9d5369517f464bd734395872ac21
Walter Santiago <wsantiagog1@gmail.com>
2018-04-29 19:34:57 -0500

    Clase 10 Arrays

---
+++ operators/arrays.php
@@ -0,0 +1,38 @@
+<?php 
+
+$array1 = [
+
+ 1 => 'b',
+ 2 => 'c',
+ 0 => 'a'
+
+];
+
+/*$array2 = [
+
+ 3 => 'd',
+ 4 => 'e',
+
+];
+
+$result = $array1 + $array2;
+var_dump($result);*/
+
+$array2 = ['a', 'b', 'c'];
+
+var_dump($array1 === $array2);
+
+
+/*
+$array1 = [
+    1 => 'b',
+    2 => 'c',
+    0 => 'a'
+];
+
+$array2 = ['a', 'b', 'c'];
+
+var_dump($array1 == $array2);
+*/
+
+?>
