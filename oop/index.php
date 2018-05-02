e863e02253953b2881e2532a2677ac237007e32a
Walter Santiago <wsantiagog1@gmail.com>
2018-05-02 13:08:51 -0500

    Clase 21 Introducción a Programación Orientada a Objetos

---
+++ oop/index.php
@@ -0,0 +1,42 @@
+<?php 
+
+/**
+* Class Car
+*/
+class Car 
+{
+	//public $owner;
+	//private $owner = 'Walter'; //Eliminamos esta linea, porque no queremos que sea estática la asignación de esta variable, todos los vehículos que creemos no van hacer propiedad de walter, entonces debemos crear un método set
+	private $owner;
+
+	public function move(){
+		echo 'moving<br>';
+	}
+
+	public function getOwner() {
+		return $this->owner;
+	}
+
+	public function setOwner($owner) {
+		$this->owner = $owner;
+	}
+
+}
+
+echo 'Class Car<br>';
+
+$car = new Car();
+$car2 = new Car();
+
+$car->move();
+$car->setOwner('Walter');
+$car2->setOwner('Max');
+
+//Podemos acceder de esta forma cuando la variable es publica
+//$car->owner = 'Alex';
+
+//Para obtener nuestra variable privada, debemos realizar el get del atributo
+echo 'Owner car: ' . $car->getOwner() . '<br>';
+echo 'Owner car2: ' . $car2->getOwner();
+
+?>
