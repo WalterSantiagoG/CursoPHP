8630859ea46a28bf9ce24d124954a4ea4b339d77
Walter Santiago <wsantiagog1@gmail.com>
2018-05-02 16:08:47 -0500

    Clase 25 Static

--- oop/index.php
+++ oop/index.php
@@ -11,11 +11,11 @@ echo 'Class Car<br>';
 
 $car = new Car('Walter');
 $car->move();
-echo 'Owner car: ' . $car->getOwner() . '<br>';
+//echo 'Owner car: ' . $car->getOwner() . '<br>';
 
-echo '<br>Class truck<br>';
-$truck = new Truck('Max', 'Pickup');
-$truck->move();
+echo '<br>Class truck 1<br>';
+$truck1 = new Truck('Max', 'Pickup');
+$truck1->move();
 /*$car->setOwner('Walter');
 $car2->setOwner('Max');*/
 
@@ -23,6 +23,12 @@ $car2->setOwner('Max');*/
 //$car->owner = 'Alex';
 
 //Para obtener nuestra variable privada, debemos realizar el get del atributo
-echo 'Owner truck: ' . $truck->getOwner();
+//echo 'Owner truck: ' . $truck->getOwner();
+
+echo '<br>Class truck 2<br>';
+$truck2 = new Truck('Max', 'Pickup');
+$truck2->move();
+
+echo '<br>Total Trucks: ' . Truck::getTotal() . '<br>';;
 
 ?>
--- oop/vehicles/Truck.php
+++ oop/vehicles/Truck.php
@@ -6,6 +6,8 @@ require_once 'VehicleBase.php';
 
 class Truck extends VehicleBase {
 
+	//public static $count = 0;
+	private static $count = 0;
 	private $type;
 
 	public function __construct($ownerName, $type)
@@ -13,10 +15,15 @@ class Truck extends VehicleBase {
 		$this->type = $type;
 		//parent::__construct($ownerName);
 		$this->owner = $ownerName;
+		self::$count++;
 	}
 
 	public function move(){
 		echo 'Truck ' . $this->type . ': moving<br>';
+	}
+
+	public static function getTotal(){
+		return self::$count;
 	}
 }
 
