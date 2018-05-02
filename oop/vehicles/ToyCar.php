700f055d5b0230882da964195213d532f11ae65f
Walter Santiago <wsantiagog1@gmail.com>
2018-05-02 16:22:54 -0500

    Clase 28 Excepciones

---
+++ oop/vehicles/ToyCar.php
@@ -0,0 +1,23 @@
+<?php 
+
+namespace Vehicles;
+
+require_once 'VehicleBase.php';
+
+/**
+* Class Vehicle
+*/
+class ToyCar extends VehicleBase {
+	public function move(){
+		echo $this->startEngine() . '<br>';
+		echo 'Car: moving<br>';
+	}
+
+	public function startEngine(){
+		throw new \Exception("Engine not found");
+	}
+
+}
+
+
+?>
