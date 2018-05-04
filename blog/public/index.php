d8954006c0e0895ceff3ccec45824e23a37dbb0e
Walter Santiago <wsantiagog1@gmail.com>
2018-05-03 20:31:09 -0500

    Clase 42 Introducción a Router

--- blog/public/index.php
+++ blog/public/index.php
@@ -6,12 +6,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once '../vendor/autoload.php';
+
 //Solo aparece en nuestro Front Controller y no en cada una de las paginas
 include_once '../config.php';
 
 $route = $_GET['route'] ?? '/';
 
-switch ($route) {
+/*switch ($route) {
 	case '/':
 		require '../index.php';
 		break;
@@ -23,4 +25,23 @@ switch ($route) {
 	case '/admin/posts':
 		require '../admin/posts.php';
 		break;
-}
+}*/
+
+use Phroute\Phroute\RouteCollector;
+
+$router = new RouteCollector();
+$router->get('/',function() use ($pdo){
+	$query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+	$query->execute();
+
+	$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+	include '../views/index.php';
+});
+
+//Despues de la ruta dispatcher: es el objeto que va tomar la ruta que nos esta llegando y va a mandar a llamar el metodo que realmente necesita 
+$dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+
+//Respuesta dispatcher, REQUEST_METHOD Vamos a recibir el metodo que estamos utilizando sea GET o POST
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+
+echo $response;
--- blog/vendor/composer/autoload_static.php
+++ blog/vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit8c199a4812989a6b121206f096449e18
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit8c199a4812989a6b121206f096449e18::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit8c199a4812989a6b121206f096449e18::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
