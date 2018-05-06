ab1e5232744533dc565190dd6fb060bbb8a98b80
Walter Santiago <wsantiagog1@gmail.com>
2018-05-06 18:04:19 -0500

    Clase 48 Templates de vistas con Twig

--- blog/app/controllers/IndexController.php
+++ blog/app/controllers/IndexController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-class IndexController {
+class IndexController extends BaseController {
 
     public function getIndex() {
 
@@ -12,7 +12,7 @@ class IndexController {
 	    $query->execute();
 
 	    $blogPosts = $query->fetchAll(\PDO::FETCH_ASSOC);
-	    return render('../views/index.php', ['blogPosts' => $blogPosts]);
+	    return $this->render('index.twig', ['blogPosts' => $blogPosts]);
     }
 
 }
