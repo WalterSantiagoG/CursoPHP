8735131c916a764816ab6e30ea969beef07bc70c
Walter Santiago <wsantiagog1@gmail.com>
2018-05-06 23:01:57 -0500

    Clase 56 Logout de usuarios

--- blog/app/controllers/AuthController.php
+++ blog/app/controllers/AuthController.php
@@ -38,4 +38,9 @@ class AuthController extends BaseController {
         ]);
     }
 
+    public function getLogout(){
+        unset($_SESSION['userId']);
+        header('Location:' . BASE_URL . 'auth/login');
+    }
+
 }
