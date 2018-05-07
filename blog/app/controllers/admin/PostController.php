138791b768f69021171a8034565ccb190541070e
Walter Santiago <wsantiagog1@gmail.com>
2018-05-06 23:53:47 -0500

    Clase 58 Subir archivos al servidor

--- blog/app/controllers/admin/PostController.php
+++ blog/app/controllers/admin/PostController.php
@@ -33,6 +33,10 @@ class PostController extends BaseController {
                 'title' => $_POST['title'],
                 'content' => $_POST['content']
             ]);
+            if ($_POST['img']) {
+                $blogPost->img_url = $_POST['img'];
+            }
+
             $blogPost->save();
             $result = true;
         }else{
--- blog/app/models/BlogPost.php
+++ blog/app/models/BlogPost.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 //extends Model, para indicar que esta clase se va a comportar como un modelo de Eloquent
 class BlogPost extends Model {
     protected $table = 'blog_posts';
-    protected $fillable = ['title','content'];
+    protected $fillable = ['title','content', 'img_url'];
 }
