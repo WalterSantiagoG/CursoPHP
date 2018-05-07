8a293be645d2bcbae791605cca80cec8dc34ffe0
Walter Santiago <wsantiagog1@gmail.com>
2018-05-06 21:33:19 -0500

    Clase 53 Agregando un modelo para los usuarios del blog

---
+++ blog/app/controllers/admin/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
+use App\Models\User;
+
+class UserController extends BaseController {
+
+    public function getIndex() {
+        $users = User::all();
+        return $this->render('admin/users.twig',[
+            'users' => $users
+        ]);
+    }
+
+}
