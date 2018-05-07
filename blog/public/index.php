5bd09e21cb6a6bc598e6dc65d35a8be132054c48
Walter Santiago <wsantiagog1@gmail.com>
2018-05-06 23:21:42 -0500

    Clase 57 Protege ciertas rutas con middlewares y filtros

--- blog/public/index.php
+++ blog/public/index.php
@@ -46,11 +46,21 @@ use Phroute\Phroute\RouteCollector;
 
 $router = new RouteCollector();
 
-$router->controller('/auth', App\Controllers\AuthController::class);
-$router->controller('/admin', App\Controllers\Admin\IndexController::class);
+//https://packagist.org/packages/phroute/phroute //filter phroute Protege ciertas rutas con middlewares y filtros
+$router->filter('auth', function(){
+    if (!isset($_SESSION['userId'])) {
+        header('Location:'.BASE_URL.'auth/login');
+        return false;
+    }
+});
 
-$router->controller('/admin/posts', App\Controllers\Admin\PostController::class);
-$router->controller('/admin/users', App\Controllers\Admin\UserController::class);
+$router->controller('/auth', App\Controllers\AuthController::class);
+
+$router->group(['before' => 'auth'], function($router){
+    $router->controller('/admin', App\Controllers\Admin\IndexController::class);
+    $router->controller('/admin/posts', App\Controllers\Admin\PostController::class);
+    $router->controller('/admin/users', App\Controllers\Admin\UserController::class);
+});    
 
 $router->controller('/', App\Controllers\IndexController::class);
 
